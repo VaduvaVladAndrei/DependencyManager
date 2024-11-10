64f4bb092abc606d73064445decf2444f88c574e
VaduvaVladAndrei <vlandreiv@gmail.com>
2024-11-10 16:10:08 +0200

    updated project explorer

--- ProjectExplorer.py
+++ ProjectExplorer.py
@@ -1,3 +1,4 @@
+from DependencyManager import DependencyManager
 from PackageReader import PackageReader
 
 
@@ -5,7 +6,13 @@ class ProjectExplorer():
     def __init__(self, project_path):
         self.project_path = project_path
         self.package_reader=PackageReader(project_path)
+        self.dependency_manager=DependencyManager(project_path)
+
+    def get_dependencies(self,package_name):
+        installed_packages=self.package_reader.read_packages()
+        if package_name not in installed_packages:
+
 
 if __name__ == '__main__':
-    p=ProjectExplorer('C:/Users/vland/source/repos/proiectcoo/')
+    p=ProjectExplorer('C:/Users/vland/source/repos/depmanagertestproject')
     p.package_reader.read_packages()
